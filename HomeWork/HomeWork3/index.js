ea6682b4e313bd433b8c32fb8732c9c6521c769f
Denis <den24.03@mail.ru>
2024-01-22 18:49:03 +0500

    add HomeWork3

---
+++ HomeWork/HomeWork3/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+let counterVisits = {};
+const dataCounterVisits = fs.readFileSync('counter.json');
+counterVisits = JSON.parse(dataCounterVisits);
+
+function SaveCounter(url) {
+  if (url == '/') {
+    counterVisits.urlIndex += 1;
+  } else if (url == '/about') {
+    counterVisits.urlAbout += 1;
+  }
+
+  const counterJson = JSON.stringify(counterVisits, null, 2);
+  console.log(counterJson);
+  fs.writeFileSync('counter.json', counterJson);
+}
+
+app.get('/', (req, res) => {
+  res.send(`
+  <h1 style="text-align: center;">Главная страница</h1><br>
+  <span style="display: block;margin-bottom: 15px;text-align: right;">Просмотров:${counterVisits.urlIndex}</span><br>
+  <a href="/about" style="display: block;text-align: center;color: blue;font-weight: 700">Ссылка на страницу/about</a>`);
+  SaveCounter('/');
+})
+
+app.get('/about', (req, res) => {
+  res.send(`
+  <h1 style="text-align: center;">Cтраница о нас</h1><br>
+  <span style="display: block;margin-bottom: 15px;text-align: right;">Просмотров:${counterVisits.urlAbout}</span><br>
+  <a href="/" style="display: block;text-align: center;color: blue;font-weight: 700">Ссылка на главную страницу</a>`);
+  SaveCounter('/about');
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('Сервер запущен!');
+})
